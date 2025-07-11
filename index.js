0521429020b143fe562a95743c3e099aa4e480bd
risa walles <rmw395@nau.edu>
2025-07-10 23:25:52 -0700

    stuff mostly works

--- index.js
+++ index.js
@@ -42,22 +42,38 @@ app.get("/create", (req, res) => {
     res.render("create.ejs");
 });
 
-app.post("/submit", (req, res) => {
-    const postAuthor = req.body['author'];
+app.post("/submit", async (req, res) => {
     const postTitle = req.body['title'];
     const postContent = req.body['content'];
+
+    let displayName = await db.query("SELECT name FROM users WHERE user_id = $1", 
+        [current_user]
+    );
+
+    displayName = displayName.rows[0].name;
+
     const newPost = {
-        creator_name: postAuthor,
         title: postTitle,
         body: postContent,
-        // pDate: today.toDateString()
+        creator_name: displayName,
+        creator_user_id: current_user //,
+        //date: today.toDateString()
     };
 
-    posts.push(newPost);
+    // posts.push(newPost);
+
+    // temp
+    let date = "2025-07-10 10:45:03";
+
+    await db.query("INSERT INTO blogs (creator_name, creator_user_id, title, body, date_created) VALUES ($1, $2, $3, $4, $5)",
+        [displayName, current_user, postTitle, postContent, date]
+    );
     res.redirect("/");
 });
 
 app.get("/edit/:id", (req, res) => {
+    // TODO: only edit if correct user
+
     const postId = req.params.id;
     const postToEdit = posts[postId];
 
@@ -67,15 +83,26 @@ app.get("/edit/:id", (req, res) => {
 app.post("/edit/:id", (req, res) => {
     const postId = req.params.id;
 
-    posts[postId].creator_name = req.body.author;
     posts[postId].title = req.body.title;
     posts[postId].body = req.body.content;
 
     res.redirect("/");
 });
 
-app.post("/delete/:id", (req, res) => {
-    posts.splice(req.params.id, 1);
+app.post("/delete/:id", async (req, res) => {
+    if (current_user = req.body.user_id) {
+        // posts.splice(req.params.id, 1);
+
+        // TODO: get id correctly (hidden form value?)
+
+        try { 
+            await db.query("DELETE FROM blogs WHERE blog_id = $1",
+                [req.params.id]
+            );
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     res.redirect("/");
 });
@@ -111,13 +138,18 @@ app.post("/login", async (req, res) => {
 
     try {
         // log in
-        await db.query("SELECT * FROM users WHERE user_id = $1 AND password = $2",
+        const user = await db.query("SELECT * FROM users WHERE user_id = $1 AND password = $2",
             [username, password]
         );
 
-        current_user = username;
+        if (user.rowCount > 0) { // !(current_user = null)
+            current_user = username;
 
-        res.redirect("/");
+            res.redirect("/");
+        } else {
+            // TODO: prompt user to try again
+        }
+
     } catch (err) {
         console.log(err);
     }
